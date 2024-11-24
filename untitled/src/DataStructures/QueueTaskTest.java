b76bb4d109a6026266c562c080d338b31006058e
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2024-11-25 00:50:57 +0100

    data structure tasks update

--- untitled/src/DataStructures/QueueTaskTest.java
+++ untitled/src/DataStructures/QueueTaskTest.java
@@ -16,5 +16,34 @@ public class QueueTaskTest {
         assertEquals(3, queue.getQueue().length);
     }
 
+    @Test
+    public void testThatFunctionDequeuesElementsFromAQueue() {
+        Queues queue = new Queues(3);
+        assertFalse(queue.isEmpty());
+        queue.enqueue(2);
+        queue.enqueue(5);
+        queue.enqueue(8);
+        assertEquals(3, queue.getQueue().length);
+        queue.dequeue();
+        queue.dequeue();
+        int expected = 5;
+        assertEquals(expected, queue.getValue());
+    }
+
+    @Test
+    public void testThatFunctionDisplaysMostRecentDequeuedElement() {
+        Queues queue = new Queues(3);
+        assertFalse(queue.isEmpty());
+        queue.enqueue(2);
+        queue.enqueue(5);
+        queue.enqueue(8);
+        assertEquals(3, queue.getQueue().length);
+        queue.dequeue();
+        int expected = 2;
+        assertEquals(expected, queue.getValue());
+
+    }
+
+
 
 }
--- untitled/src/DataStructures/Queues.java
+++ untitled/src/DataStructures/Queues.java
@@ -5,6 +5,7 @@ public class Queues {
     private int size;
     private int[] queue;
     int count;
+    int value = 0;
 
     public Queues(int size) {
         this.size = size;
@@ -26,5 +27,26 @@ public class Queues {
 
     public int[] getQueue(){
         return this.queue;
+    }
+
+    public int dequeue() {
+
+        int front = 0;
+        value = this.queue[front];
+        for(int index = 0; index < size - 1; index++) {
+            this.queue[index] = this.queue[index + 1];
+        }
+        size--;
+
+        return value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int front() {
+        value = this.queue[0];
+        return value;
     }
 }
