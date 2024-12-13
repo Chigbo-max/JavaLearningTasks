78e34425de78c40e58313d050db0e3e8ebe2e51f
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2024-12-13 06:08:02 +0100

    caesar cypher cryptography

--- CaesarCypherEncryption/src/caesarCypher/EncryptionAndDecryption.java
+++ CaesarCypherEncryption/src/caesarCypher/EncryptionAndDecryption.java
@@ -3,11 +3,11 @@ package caesarCypher;
 public class EncryptionAndDecryption {
 
 
-    public static int getValueOf5Modulus26(int number) {
+    public static int getValueOf5Modulus26(int key) {
         int result = 0;
-        if(number < 26)
-            return number;
-        result = number % 26;
+        if(key < 26)
+            return key;
+        result = key % 26;
         return result;
     }
 
@@ -15,36 +15,79 @@ public class EncryptionAndDecryption {
         int[] result = new int[plainText.length()];
         for(int index = 0; index < plainText.length(); index++) {
             char character = plainText.charAt(index);
-            if(Character.isLetter(character)) {
-                character = Character.toLowerCase(character);
-                int number = character - 'a';
-                result[index] = number;
-            }
+            validateCharacter(character);
+            character = Character.toLowerCase(character);
+            int number = character - 'a';
+            result[index] = number;
         }
         return result;
     }
 
-    public int[] getModulusValueForEachCharacter(String plainText) {
-        int[] characterValue = getValueOfCharacter(plainText);
-        int[] modulusValue = new int[characterValue.length];
-        int number = 3;
-        for(int index = 0; index < modulusValue.length; index++) {
-            if((characterValue[index] + number)  < 26)
-                modulusValue[index] = characterValue[index] + number;
+    private static void validateCharacter(char character) {
+        if(!Character.isLetter(character)) {
+            throw new IllegalArgumentException("Invalid character");
+        }
 
-            modulusValue[index] = (characterValue[index] + number) % 26;
+    }
+
+    public int[] getEncryptedModulusValueForEachCharacter(String plainText) {
+        int[] characterValues = getValueOfCharacter(plainText);
+        int[] modulusValue = new int[characterValues.length];
+        int key = 3;
+        for(int index = 0; index < modulusValue.length; index++) {
+            validateEncryptedModulusValue(characterValues, index, key, modulusValue);
         }
         return modulusValue;
     }
 
+    private static void validateEncryptedModulusValue(int[] characterValues, int index, int key, int[] modulusValue) {
+        if((characterValues[index] + key)  < 26)
+            modulusValue[index] = characterValues[index] + key;
+
+        modulusValue[index] = (characterValues[index] + key) % 26;
+    }
+
 
     public char[] getEncryptedResult(String plainText) {
-        int[] modulusValue = getModulusValueForEachCharacter(plainText);
-        char[] encryptedValues = new char[modulusValue.length];
-        for(int index = 0; index < modulusValue.length; index++) {
-            encryptedValues[index] = (char) (modulusValue[index] + 'a');
+        int[] modulusValues = getEncryptedModulusValueForEachCharacter(plainText);
+        char[] encryptedValues = new char[modulusValues.length];
+        for(int index = 0; index < modulusValues.length; index++) {
+            encryptedValues[index] = (char) (modulusValues[index] + 'a');
         }
         return encryptedValues;
-        
+    }
+
+
+    public int[] getDecryptedModulusValueForEachCharacter(String plainText) {
+        int[] characterValues = getValueOfCharacter(plainText);
+        int[] decryptedModulusValues = new int[characterValues.length];
+        int key = 3;
+        for(int index = 0; index < decryptedModulusValues.length; index++) {
+            validateEncryptionModulusValue(characterValues, index, key, decryptedModulusValues);
+        }
+        return decryptedModulusValues;
+    }
+
+    private static void validateEncryptionModulusValue(int[] characterValues, int index, int key, int[] decryptedModulusValues) {
+        int decryptionValue = characterValues[index] - key;
+
+        if(decryptionValue  < 26 && characterValues[index] >= key){
+            decryptedModulusValues[index] = decryptionValue;
+        }
+        else {
+            decryptedModulusValues[index] = decryptionValue % 26;
+        }
+        if(decryptionValue < 0){
+            decryptedModulusValues[index] = 26 + decryptionValue;
+        }
+    }
+
+    public char[] getDecryptedResult(String plainText) {
+        int[] decryptedModulusValues = getDecryptedModulusValueForEachCharacter(plainText);
+        char[] decryptedValues = new char[decryptedModulusValues.length];
+        for(int index = 0; index < decryptedValues.length; index++) {
+            decryptedValues[index] = (char) (decryptedModulusValues[index] + 'a');
+        }
+        return decryptedValues;
     }
 }
--- CaesarCypherEncryption/test/caesarCypher/EncryptionAndDecryptionTest.java
+++ CaesarCypherEncryption/test/caesarCypher/EncryptionAndDecryptionTest.java
@@ -2,43 +2,70 @@ package caesarCypher;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class EncryptionAndDecryptionTest {
 
     @Test
     public void testThat5modulus26Returns5(){
         int number = 5;
-        int result = EncryptionAndDecryption.getValueOf5Modulus26(number);
-        int expectedResult = 5;
-        assertEquals(result,expectedResult);
+        int actual = EncryptionAndDecryption.getValueOf5Modulus26(number);
+        int expected = 5;
+        assertEquals(actual,expected);
+        
     }
 
     @Test
     public void testThatTheNumericValueForCharacterIsReturned(){
         String plainText = "CALIFORNIA";
         EncryptionAndDecryption  encryptionAndDecryption = new EncryptionAndDecryption();
-        int[] result = encryptionAndDecryption.getValueOfCharacter(plainText);
-        int[] expectedResult = {2,0,11,8,5,14,17,13,8,0};
-        assertArrayEquals(expectedResult, result);
+        int[] actual = encryptionAndDecryption.getValueOfCharacter(plainText);
+        int[] expected = {2,0,11,8,5,14,17,13,8,0};
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testThatTheModulus26ValueForCharacterIsReturned(){
+    public void testThatExceptionIsThrowForNonLetter(){
+        String plainText = "CALIFOR32A";
+        EncryptionAndDecryption  encryptionAndDecryption = new EncryptionAndDecryption();
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, ()->encryptionAndDecryption.getValueOfCharacter(plainText));
+        assertEquals(illegalArgumentException.getMessage(),"Invalid character");
+    }
+
+    @Test
+    public void testThatTheEncryptedModulus26ValueForCharacterIsReturned(){
         String plainText = "CALIFORNIA";
         EncryptionAndDecryption  encryptionAndDecryption = new EncryptionAndDecryption();
-        int[] result = encryptionAndDecryption.getModulusValueForEachCharacter(plainText);
-        int[] expectedResult = {5,3,14,11,8,17,20,16,11,3};
-        assertArrayEquals(expectedResult, result);
+        int[] actual = encryptionAndDecryption.getEncryptedModulusValueForEachCharacter(plainText);
+        int[] expected = {5,3,14,11,8,17,20,16,11,3};
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testThatModulusValueReturnsTheEncryptedValue(){
         String plainText = "CALIFORNIA";
         EncryptionAndDecryption  encryptionAndDecryption = new EncryptionAndDecryption();
-        char[] result = encryptionAndDecryption.getEncryptedResult(plainText);
-        char[] expectedResult = {'f','d','o','l','i','r','u','q','l','d'};
-        assertArrayEquals(expectedResult, result);
+        char[] actual = encryptionAndDecryption.getEncryptedResult(plainText);
+        char[] expected = {'f','d','o','l','i','r','u','q','l','d'};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatTheDecryptedModulus26ValueForCharacterIsReturned(){
+        String plainText = "CALIFORNIA";
+        EncryptionAndDecryption  encryptionAndDecryption = new EncryptionAndDecryption();
+        int[] actual = encryptionAndDecryption.getDecryptedModulusValueForEachCharacter(plainText);
+        int[] expected = {25,23,8,5,2,11,14,10,5,23};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatModulusValueReturnsTheDecryptedValue(){
+        String plainText = "CALIFORNIA";
+        EncryptionAndDecryption  encryptionAndDecryption = new EncryptionAndDecryption();
+        char[] actual = encryptionAndDecryption.getDecryptedResult(plainText);
+        char[] expected = {'z', 'x', 'i', 'f', 'c', 'l', 'o', 'k', 'f','x'};
+        assertArrayEquals(expected, actual);
     }
 }
